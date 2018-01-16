ae244d87c320a24a351cf400de101feda7d8f078
sudokarst <sudokarst@gmail.com>
2018-01-15 19:48:13 -0500

    updated main.py to connect to db; added class Task(db.Model)

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:beproductive@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
+
+
 
 tasks = []
 
@@ -15,4 +29,5 @@ def index():
     return render_template('todos.html',title="Get It Done!", tasks=tasks)
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
