0a06c4a1ed210ad336fff9a4b8bdc791ecd4e259
sudokarst <sudokarst@gmail.com>
2018-02-05 01:14:21 -0500

    login and signup work; passwords still plaintext

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, session, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -17,25 +19,43 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+    def __repr__(self):
+        return "<User={id={self.id}, username='{username}'}>"
+
 class BlogPost(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     body = db.Column(db.String(4095))
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
 
     def __init__(self, title, body, author_id):
         self.title = title
         self.body = body
         self.author_id = author_id
 
+def is_invalid_username(username_attempt):
+    """returns False if username_attempt is a valid username,
+        otherwise returns a helpful error message"""
+    return (False if re.match("^[a-zA-Z][a-zA-Z0-9_]{2,19}$",
+            username_attempt) else
+            """username must be 3-20 characters in length, first character in
+            [a-zA-Z], remaining characters in [a-zA-Z0-9_]""")
+
+def is_invalid_password(password_attempt):
+    """returns False if password_attempt is a valid password,
+        otherwise returns a helpful error message"""
+    return (False if re.match("^\S{3,20}$", password_attempt) else
+        "password must be 3-20 characters in length, with no whitespace")
+
 @app.route('/')
 def index():
     return redirect('/blog')
 
+
 @app.route('/blog')
 def display_all_posts():
-
     post_id = request.args.get('id')
     print("post id {0!r}".format(post_id))
     if post_id:
@@ -45,6 +65,96 @@ def display_all_posts():
     posts = BlogPost.query.all()
     return render_template('blog.html',title="It's Alive!",
                             posts=posts)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def new_user():
+    # User enters new, valid username, a valid password, and verifies password correctly and is redirected to the '/newpost' page with their username being stored in a session.
+    # User leaves any of the username, password, or verify fields blank and gets an error message that one or more fields are invalid.
+    # User enters a username that already exists and gets an error message that username already exists.
+    # User enters different strings into the password and verify fields and gets an error message that the passwords do not match.
+    # User enters a password or username less than 3 characters long and gets either an invalid username or an invalid password message.
+
+    if request.method == 'GET':
+        return render_template('signup.html')
+
+    validated = True
+
+    #
+    # username validation
+    #
+    username_attempt = request.form.get('username', '').strip()
+    username_errmsg = is_invalid_username(username_attempt)
+    if username_errmsg: # username is empty or no good
+        validated = False
+    if User.query.filter_by(username=username_attempt).first() is not None:
+        validated = False
+        username_errmsg = "username already exists"
+    valid_username = username_attempt if validated else None
+    
+    #
+    # password validation
+    #
+    password_errmsg = ''
+    password2_errmsg = ''
+    password_attempt = request.form.get('password', '').strip()
+    password2_attempt = request.form.get('password2', '').strip()
+    password_error = is_invalid_password(password_attempt)
+    if password_error:
+        validated = False
+        password_errmsg = password_error
+    elif password_attempt != password2_attempt:
+        validated = False
+        password_errmsg = "good password ..."
+        password2_errmsg = "but they don't match!"
+    valid_password = password_attempt if validated else None
+    
+    if not validated:
+        return render_template('signup.html',
+                    username_invalid=username_errmsg,
+                    password_invalid=password_errmsg,
+                    password2_invalid=password2_errmsg)
+    elif valid_username and valid_password:
+        new_user = User(valid_username, valid_password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['username'] = valid_username
+        return redirect('/newpost')
+    else:
+        return 
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def do_login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username_attempt = request.form.get('username')
+    if not username_attempt:
+        return render_template('login.html')
+
+    username_attempt = username_attempt.strip()
+    username_error = is_invalid_username(username_attempt)
+    if username_error:
+        return render_template('login.html', errmsg=username_error)
+    
+    user = User.query.filter_by(username=username_attempt).first()
+    # User enters a username that is stored in the database with the correct password and is redirected to the /newpost page with their username being stored in a session.
+    if user:
+        # TODO: # User enters a username that is stored in the database with an incorrect password and is redirected to the /login page with a message that their password is incorrect.
+        valid_password = False
+
+        password_attempt = request.form.get('password')
+        if not password_attempt or password_attempt != user.password:
+            return render_template('login.html',
+                                    username=username_attempt,
+                                    invalid_password="invalid password")
+        else:
+            return redirect('/newpost')
+    else:
+        return render_template('login.html', errmsg="username does not exist")
+        # User tries to login with a username that is not stored in the database and is redirected to the /login page with a message that this username does not exist.
+    # User does not have an account and clicks "Create Account" and is directed to the /signup page.
 
 @app.route('/newpost', methods=['GET', 'POST'])
 def publish():
@@ -59,6 +169,10 @@ def publish():
     else:
         return render_template('newpost.html')
 
+
+# >>> import secrets
+# >>> secrets.token_urlsafe(24)
+app.secret_key =  'mxLDMoEHmbuewIWDQmOl1oFgpALUScrb'
 
 if __name__ == "__main__":
     app.run()
